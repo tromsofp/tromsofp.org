4fe04a7e8b2e1d53aea67e51db91c395a5b4cd93
Jonas Juselius <jonas.juselius@itpartner.no>
2016-06-16 14:19:49 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# TromsoFP
+
+The source code behind http://tromsofp.org.
+Just fork, clone, and contribute.
---
+++ thugster.hs
@@ -0,0 +1,19 @@
+import Data.List
+import Data.Char
+import Data.Bits
+import Numeric
+
+encr = concatMap (flip showHex "" . xor 0xff . ord)
+    . unwords
+    . map reverse
+    $ words "Join now and become functionally inclined today!"
+
+decr = foldl ((. reverse) . (++)) mempty
+    . intersperse [chr $ 2^5]
+    . words
+    . map (chr . xor 0xff . fst . head . readHex)
+    . takeWhile (not . null) $ unfoldr (Just . splitAt 2) encr
+
+main = do
+    putStrLn encr
+    putStrLn decr
